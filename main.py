1d54737c93c49a1913da2d565f876dcc436bc1a2
Juan Diego Varon <jdiegov15@gmail.com>
2022-11-27 22:07:06 -0500

    ImplementaciÂón de Main y detalles en gráfica para mostrar los estados

--- main.py
+++ main.py
@@ -1,2 +1,26 @@
 from Models.control import Control
+from View.grafica import Grafica
 
+
+class Main:
+    '''Ejecuta el programa'''
+    control: Control
+    grafica: Grafica = None
+    listaNoTerminales: str
+    listaTerminales: str
+    listaProducciones: str
+
+    def __init__(self) -> None:
+        self.grafica = Grafica(self.comenzarPrograma)
+
+    def comenzarPrograma(self):
+        '''Se ejecuta el programa, obteniendo los valores desde 
+        la interfaz gráfica'''
+        self.listaNoTerminales = ''
+        self.listaTerminales = ''
+        self.listaProducciones = ''
+
+        control = Control(self.listaNoTerminales,
+                          self.listaTerminales, self.listaProducciones)
+
+        return control.estados
